a7344d753ab62aab4fea5217bd83e136b4614ac4
LMedina96 <lucasgmedina96@gmail.com>
2022-05-15 00:35:06 -0300

    First edition

--- my-repository/src/App.vue
+++ my-repository/src/App.vue
@@ -1,119 +1,54 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from '@/components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  <div id="main">
+    <h1>Lucas Medina Portfolio</h1>
+    <div id="principal" class=".container-fluid rounded">
+      <Biography title="Biografia" />
     </div>
-  </header>
-
-  <RouterView />
+  </div>
 </template>
 
+
+
+
+
+<script>
+import Biography from "./components/Biography.vue";
+
+export default {
+  components: {
+    Biography,
+  },
+};
+</script>
+
+
+
+
+
 <style>
-@import '@/assets/base.css';
-
-#app {
-  max-width: 1280px;
-  margin: 0 auto;
-  padding: 2rem;
-
-  font-weight: normal;
+* {
+  box-sizing: content-box;
 }
 
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+body {
+  background-color: black;
+  color: white;
+  background-image: url("./resources/images/backgroundPortfolio.jpg");
+  background-size: cover;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-a,
-.green {
-  text-decoration: none;
-  color: hsla(160, 100%, 37%, 1);
-  transition: 0.4s;
-}
-
-@media (hover: hover) {
-  a:hover {
-    background-color: hsla(160, 100%, 37%, 0.2);
-  }
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
+#main {
   text-align: center;
-  margin-top: 2rem;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
+h1 {
+  background-color: rgb(56, 56, 56);
+  padding: 1rem;
 }
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  body {
-    display: flex;
-    place-items: center;
-  }
-
-  #app {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    padding: 0 2rem;
-  }
-
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+#principal {
+  background-color: rgba(61, 61, 61, 0.8);
+  margin: 5rem 2rem;
+  min-height: 70vh;
 }
 </style>
