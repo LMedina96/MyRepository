9d039c0acbcf987fcfcb4d61ea3a7265c0b6a06b
LMedina96 <lucasgmedina96@gmail.com>
2022-05-16 21:46:44 -0300

    Pre main worked

--- my-repository/src/App.vue
+++ my-repository/src/App.vue
@@ -1,10 +1,82 @@
 <template>
-  <div id="main">
-    <h1>Lucas Medina Portfolio</h1>
-    <div id="principal" class=".container-fluid rounded">
-      <Biography title="Biografia" />
+  <main id="main">
+    <!--!HEADER-->
+    <header>
+      <h1>Lucas Medina Portfolio</h1>
+      <nav>
+        <form class="container-fluid justify-content-start">
+          <button
+            class="
+              btn btn-sm btn-outline-secondary
+              border border-success
+              p-2
+              mb-2
+            "
+            type="button"
+          >
+            Biografia
+          </button>
+          <button
+            class="
+              btn btn-sm btn-outline-secondary
+              border border-success
+              p-2
+              mb-2
+            "
+            type="button"
+          >
+            Conocimientos
+          </button>
+          <button
+            class="
+              btn btn-sm btn-outline-secondary
+              border border-success
+              p-2
+              mb-2
+            "
+            type="button"
+          >
+            Portfolio
+          </button>
+        </form>
+      </nav>
+    </header>
+
+    <!--!BODY-->
+    <div id="principal" class=".container-fluid rounded row">
+      <div class="left-arrow col-1">&lt;</div>
+      <div id="photo" class="col-3">
+        <img src="./resources/images/mypicturefinal.jpg" alt="Cargando imagen..." />
+      </div>
+      <div class="container col-6">
+        <Biography title="Biografia" />
+      </div>
+      <div class="right-arrow col-1">></div>
+
+      <div class="container carrousel">
+        <button
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide-to="0"
+          class="active"
+          aria-current="true"
+          aria-label="Slide 1"
+        ></button>
+        <button
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide-to="1"
+          aria-label="Slide 2"
+        ></button>
+        <button
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide-to="2"
+          aria-label="Slide 3"
+        ></button>
+      </div>
     </div>
-  </div>
+  </main>
 </template>
 
 
@@ -13,8 +85,7 @@
 
 <script>
 import Biography from "./components/Biography.vue";
-import './assets/style.css';
-
+import "./assets/style.css";
 
 export default {
   components: {
